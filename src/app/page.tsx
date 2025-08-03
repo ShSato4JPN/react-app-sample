122023fa67ca092d08f3fc7bb0dd543eba54c22e
ShSato4JPN <shsato4.private.1221@gmail.com>
2025-08-03 17:06:38 +0900

    restore git repository

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import Dropzone from "@/components/molecules/dropzone";
-
-export default function Home() {
-  return (
-    <div className="p-10">
-      <Dropzone />
-    </div>
-  );
-}
--- src/components/atoms/shape-image.tsx
+++
@@ -1,8 +0,0 @@
-import type { ImageProps } from "next/image";
-
-type ImageShape = "circle" | "square";
-type ShapeImageProps = ImageProps & { shape?: ImageShape };
-
-export default function ShapeImage() {
-  return <div>shape-image</div>;
-}
